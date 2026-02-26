6233ec2ae169e3e0baf3b140f18cde62f973b9b4
Rawan Sotohy <143886177+Rawan-Sotohy@users.noreply.github.com>
2026-02-26 07:25:45 +0200

    Update topic_analyzer.py

--- src/topic_analyzer.py
+++ src/topic_analyzer.py
@@ -1,7 +1,7 @@
 import json
 from langchain_groq import ChatGroq
-from src.pdf_reader import load_vector_store
-from src.config import *
+from pdf_reader import load_vector_store
+from config import *
 from datetime import date
 
 def extract_topics(subject_name: str, level: str):
